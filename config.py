53dbc7205d3bbe1c27ccb0077e4fe847f81efbaa
kairatrmk <kairat.rmk@gmail.com>
2023-06-09 15:16:00 +0600

    first commit

--- config.py
+++ config.py
@@ -1,12 +1,3 @@
 tg_bot_token = "5727522993:AAGPkYVnLInznM0hPkdcbcB_L82cUQX05As"
 open_weather_token = "2e69354f6189fc7475becd2b0146b171"
 weather_tbot = "2e69354f6189fc7475becd2b0146b171"
-
-
-
-
-
-
-
-
-
